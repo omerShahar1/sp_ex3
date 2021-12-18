40c3066b2260f21f865c2121feb0ecb35a1dcf2a
omerShahar1 <93426230+omerShahar1@users.noreply.github.com>
2021-12-18 19:13:54 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -10,6 +10,13 @@
 #define z 122
 
 
+// note that in the makefile i called the final runing program "stringProg" as you requested in the instructins.
+// however, in the cheking program you created, "check_submition.py", you called the final program by the name "connections".
+// therefore the cheking program you created will only work if you change the name from "connections" to the correct name
+// you requested: "stringProg". 
+
+
+
 int size(char txt[], int i, int j)
 {
     int sum = 0, h;
--- makefile
+++ makefile
@@ -2,6 +2,10 @@ CC=gcc
 MAIN=main.o
 FLAGS= -Wall -g
 
+# note that in the makefile i called the final runing program "stringProg" as you requested in the instructins.
+# however, in the cheking program you created, "check_submition.py", you called the final program by the name "connections".
+# therefore the cheking program you created will only work if you change the name from "connections" to the correct name
+# you requested: "stringProg". 
 
 all: stringProg
 
